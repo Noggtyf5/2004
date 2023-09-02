b26bda0c8eef20af151c1bcedb01805c7f461d6b
Noggtyf5 <143862730+Noggtyf5@users.noreply.github.com>
2023-09-02 16:28:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,18 +7,18 @@ load_dotenv()
 # client vars
 API_ID = int(getenv("API_ID", "7452578"))
 API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")
-BOT_TOKEN = getenv("BOT_TOKEN", "5385370603:AAHjSbZFL1OLl_uOm3v8lS0iKSYhRJS_bKs")
-SESSION_NAME = getenv("SESSION_NAME", "AgAGFRW8AN9biZhAIB_X7rubbTCwNtc48oa-geXlaVsQbV9M1emAEV_iGPw_W-lNP1HGbOuQgF9NAUIxlp8-6rgN16-7Upuhst2wCxewyx0VP7Cxb5k_9EXap6_xeU2v4So9RZMt95XYxhGc4hWO_ikd9P46iHLMjXW8GjQAnGvY-bIrM6XqIXF1yeC3vixEa8knOHGkedZU3FYyLhEILy8TV6eH5jOGvsyw5o7xFElb2zZId_vlYO--B3xSWIMUR6zyH8wozehe9tIn0jZptYjY1rqWOMoYS9wtqvBThVazL60nrgL-KdikJC9beDJLLR0C-wzuAtARPCZ-JK6oG-gVAAAAAUM3evYA")
+BOT_TOKEN = getenv("BOT_TOKEN", "6371556042:AAFOUrS76ssLidswTwyQmmRWJO1A5Bu12NU")
+SESSION_NAME = getenv("SESSION_NAME", "1BJWap1wBu0hBRjjQwPX6Tvfv3jYEk3aRiPUO_EPDov65G6NicEtjmbW7wS7F6OOQnLZ9pTGvu39BztMpzEMhLSJb30bKOvUWqCtRTgPwIbYezA6Ln9JT7AXEwBaSVgIJMzr7Y3OYM9lH-chEDnlQjPsSmYJc3mWQsHhkkDEyqsJhwSmUOYDCHUAIn5TTMDYP96BqrpD09wgT5WyZuONdQ1rAx_sohJi44rUfag4Ob3v-PoCGhrOCyDcJkvfzQ8FGB7QPFwAO262Yh3vG0cf94ZdMNQryBJuXnFIt-NTzmZCIE2-cEF0RT_KlSJMgOtgO8cgrin8geJlqM1UKt9aikRxfmi_B6H0=-geXlaVsQbV9M1emAEV_iGPw_W-lNP1HGbOuQgF9NAUIxlp8-6rgN16-7Upuhst2wCxewyx0VP7Cxb5k_9EXap6_xeU2v4So9RZMt95XYxhGc4hWO_ikd9P46iHLMjXW8GjQAnGvY-bIrM6XqIXF1yeC3vixEa8knOHGkedZU3FYyLhEILy8TV6eH5jOGvsyw5o7xFElb2zZId_vlYO--B3xSWIMUR6zyH8wozehe9tIn0jZptYjY1rqWOMoYS9wtqvBThVazL60nrgL-KdikJC9beDJLLR0C-wzuAtARPCZ-JK6oG-gVAAAAAUM3evYA")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "c_i_i_i_i_i_i_i")
 ALIVE_NAME = getenv("ALIVE_NAME", "song")
-BOT_USERNAME = getenv("BOT_USERNAME", "W7MBoT")
+BOT_USERNAME = getenv("BOT_USERNAME", "Jeieyeheuebot")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/STKR2/2004")
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "shhejeje7")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "shhejeje8")
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
